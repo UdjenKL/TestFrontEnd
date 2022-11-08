eb8ae65773cbec5d89b777f6679517f672742acc
Yauhen Klimovich <webrubin1987@gmail.com>
2022-11-08 21:52:21 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
 import './index.scss';
+import React from "react";
 
 const questions = [
-  {
-    title: 'Как проверить значение null по типу?',
-    variants: [
-        'null == typeof null',
-        'var a = null;'+'!!!'
-        , 'приложение'],
-    correct: 0,
-  },
+    {
+        title: 'Как проверить значение null по типу?',
+        variants: [
+            'null == typeof null',
+            'var a = null ' + '\n' + '(!a && typeof a === "object")',
+            'typeof null === typeof null'],
+        correct: 1,
+    },
+
 ];
 
 function Result() {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
-    </div>
-  );
+    return (
+        <div className="result">
+            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
+            <h2>Вы отгадали 3 ответа из 10</h2>
+            <button>Попробовать снова</button>
+        </div>
+    );
 }
 
-function Game() {
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
-      </div>
-      <h1>Что такое useState?</h1>
-      <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>[]</li>
-      </ul>
-    </>
-  );
+function Game({question}) {
+    return (
+        <>
+            <div className="progress">
+                <div style={{width: '10%'}} className="progress__inner"></div>
+            </div>
+            <h1>{question.title}</h1>
+            <ul>
+                {
+                    question.variants.map((question, index) =>
+                        <li key={index}>{question}</li>)
+                }
+            </ul>
+        </>
+    );
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Game />
-       {/*<Result />*/}
-    </div>
-  );
+
+    const [step, setStep] = React.useState(0);
+    const question = questions[step];
+    console.log(question);
+
+    return (
+        <div className="App">
+            <Game question={question}/>
+            {/*<Result />*/}
+        </div>
+    );
 }
 
 export default App;
