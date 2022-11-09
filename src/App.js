a4ad8adcaaf657c1aeb865a2a065ee193cc4bb49
Yauhen Klimovich <webrubin1987@gmail.com>
2022-11-09 15:57:07 +0300

    feat: add result functional

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 
 const questions = [
     {
+        imageUrl: '../img/1.png',
         title: 'Как проверить значение null по типу?',
         variants: [
             'null == typeof null',
@@ -11,30 +12,51 @@ const questions = [
         correct: 1,
     },
     {
-        title: ''
+        imageUrl: '../img/2.png',
+        title: 'Чему равен null?',
+        variants: [
+            'console.log(typeof null == "object");',
+            'console.log(typeof null == null);',
+            'console.log(typeof null != null);'],
+        correct: 1,
+    },
+    {
+        imageUrl: '../img/2.png',
+        title: 'Чему равен null?3',
+        variants: [
+            'console.log(typeof null == "object33");',
+            'console.log(typeof null == null3);',
+            'console.log(typeof null != null3);'],
+        correct: 1,
     }
 
 ];
 
-function Result() {
+function Result({correct}) {
     return (
         <div className="result">
-            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
-            <h2>Вы отгадали 3 ответа из 10</h2>
-            <button>Попробовать снова</button>
+            <img src="./logo512.png"/>
+            <img src="./logo512.png"/>
+            <img src="./logo512.png"/>
+            <h1>Вы отгадали {correct} ответа из {questions.length}</h1>
+            <button href="/">Попробовать снова</button>
         </div>
     );
 }
 
-function Game({question, onClickVariant}) {
+function Game({step , question, onClickVariant}) {
+
+    const scaleValue = Math.round((step/questions.length)*100);
+    console.log(scaleValue);
+
     return (
         <>
             <div className="progress">
-                <div style={{width: '10%'}} className="progress__inner"></div>
+                <div style={{width: `${scaleValue}%`}} className="progress__inner"></div>
             </div>
 
             <h1>{question.title}</h1>
-             <img src="../img/1.png"/>
+            <img src={question.imageUrl}/>
             <ul>
                 {
                     question.variants.map((question, index) =>
@@ -47,21 +69,25 @@ function Game({question, onClickVariant}) {
 }
 
 function App() {
-
+    const [correct,setCorrect] = React.useState(0);
     const [step, setStep] = React.useState(0);
     const question = questions[step];
 
+
     const onClickVariant = (index) => {
-        // console.log(step,index);
-        console.log(step + 1)
+        setStep(step+1);
+        if (index === question.correct) {
+            setCorrect(correct + 1);
+        }
     }
 
     console.log(question);
 
     return (
         <div className="App">
-            <Game question={question} onClickVariant={onClickVariant}/>
-            {/*<Result />*/}
+            {
+                step != questions.length ? <Game step={step} question={question} onClickVariant={onClickVariant}/> : <Result correct={correct}/>
+            }
         </div>
     );
 }
